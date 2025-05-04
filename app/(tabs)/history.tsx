f173c5dc30793b560b1f71b1babf50d1dd933520
Nerwin Alamas <nerwinalamas@gmail.com>
2025-05-04 17:34:40 +0800

    feat: history

---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,29 @@
+import Header from "@/components/header";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function History() {
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <Header />
+
+      <ScrollView style={styles.content} showsVerticalScrollIndicator={false}>
+        <View>
+          <Text>History</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8fafc",
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+});
