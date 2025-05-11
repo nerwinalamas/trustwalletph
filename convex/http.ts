f44e9132f0d6b3f0c389366d7bb05ce9d5b48206
Nerwin Alamas <nerwinalamas@gmail.com>
2025-05-11 16:37:41 +0800

    feat: web hook for create user

---
+++ convex/http.ts
@@ -0,0 +1,75 @@
+import { httpRouter } from "convex/server";
+import { Webhook } from "svix";
+import { api } from "./_generated/api";
+import { httpAction } from "./_generated/server";
+
+const http = httpRouter();
+
+http.route({
+  path: "/clerk-webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, req) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET as string;
+
+    if (!webhookSecret) {
+      throw new Error("Missing CLERK_WEBHOOK_SECRET environment variable");
+    }
+
+    // Check headers
+    const svix_id = req.headers.get("svix-id");
+    const svix_signature = req.headers.get("svix-signature");
+    const svix_timestamp = req.headers.get("svix-timestamp");
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("Error occurred -- no svix headers", {
+        status: 400,
+      });
+    }
+
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: any;
+
+    // verify webhook
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as any;
+    } catch (error) {
+      console.log("Error verifying webhook:", error);
+      return new Response("Error occurred", { status: 400 });
+    }
+
+    const eventType = evt.type;
+
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name, image_url } =
+        evt.data;
+
+      const email = email_addresses[0].email_address;
+      const fullName = `${first_name} ${last_name}`;
+
+      try {
+        await ctx.runMutation(api.users.createUser, {
+          clerkUserId: id,
+          firstName: first_name,
+          lastName: last_name,
+          fullName,
+          email,
+          profileImageUrl: image_url,
+        });
+      } catch (error) {
+        console.log("Error creating user:", error);
+        return new Response("Error creating user", { status: 500 });
+      }
+    }
+
+    return new Response("Webhook processed successfully", { status: 200 });
+  }),
+});
+
+export default http;
