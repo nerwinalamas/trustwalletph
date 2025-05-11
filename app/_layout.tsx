4b4f113dc3a63ca9c87ea8ba41d81aaea6dbe58c
Nerwin Alamas <nerwinalamas@gmail.com>
2025-05-11 13:46:32 +0800

    refactor: add clerk provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
