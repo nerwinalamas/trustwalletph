a6cf82dac91a9bc4a724eaaeff90163e5aea5ba4
Nerwin Alamas <nerwinalamas@gmail.com>
2025-05-17 14:01:25 +0800

    refactor: auto generated from convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as http from "../http.js";
+import type * as transactions from "../transactions.js";
 import type * as users from "../users.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   http: typeof http;
+  transactions: typeof transactions;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
