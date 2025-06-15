3cdde65543fff3d9a6a3bf985375fc9ac23a1f57
Nerwin Alamas <nerwinalamas@gmail.com>
2025-06-15 10:47:03 +0800

    refactor: auto generated from convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as http from "../http.js";
+import type * as migrations_addNewColumn from "../migrations/addNewColumn.js";
 import type * as payments from "../payments.js";
 import type * as transactions from "../transactions.js";
 import type * as users from "../users.js";
@@ -28,6 +29,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   http: typeof http;
+  "migrations/addNewColumn": typeof migrations_addNewColumn;
   payments: typeof payments;
   transactions: typeof transactions;
   users: typeof users;
