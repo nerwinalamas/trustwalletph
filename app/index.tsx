5dc5d584c4ccc7d8097b029876888f8f1a69e762
Nerwin Alamas <nerwinalamas@gmail.com>
2025-06-29 09:14:36 +0800

    refactor: remove notifications config for now

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,5 @@
-import { registerNotificationHandlers, setupNotifications } from "@/lib/notifications";
 import { Redirect } from "expo-router";
-import { useEffect } from "react";
 
 export default function Index() {
-  useEffect(() => {
-    setupNotifications();
-    const cleanup = registerNotificationHandlers();
-    return cleanup;
-  }, []);
-
   return <Redirect href="/(auth)/sign-in" />;
 }
