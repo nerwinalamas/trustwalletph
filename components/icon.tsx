4b7d6158e8097439f0431fae8767440a0f169b58
Nerwin Alamas <nerwinalamas@gmail.com>
2025-08-02 15:36:09 +0800

    feat: icon

---
+++ components/icon.tsx
@@ -0,0 +1,31 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+interface IconProps {
+  name?: keyof typeof Ionicons.glyphMap;
+  size: number;
+  color: string;
+}
+
+const Icon = ({ name, size, color }: IconProps) => {
+  return (
+    <View style={[styles.icon]}>
+      <Ionicons name={name} size={size} color={color} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 40,
+    height: 40,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+    backgroundColor: "#e0e7ff",
+  },
+});
+
+export default Icon;
